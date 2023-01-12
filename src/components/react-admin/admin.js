5e7395d60d0eb198b50446932bb63bdfdb2bda6c
carmen <carmenmquz@gmail.com>
2023-01-12 13:49:02 +0100

    Cambios en admin.js y migrations.js

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -2,7 +2,7 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from 'components/react-admin/users';
 import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
-import { MigrationList } from 'components/react-admin/migrations';
+import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
 
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
@@ -10,7 +10,7 @@ import MigrationIcon from '@mui/icons-material/Storage';
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
-const dataProvider = jsonServerProvider('http://proyecto2.test/api/records');
+const dataProvider = jsonServerProvider('http://encuentro.test/api/records');
 
 const RAdmin = () => (
   <Admin
@@ -18,7 +18,8 @@ const RAdmin = () => (
     dataProvider={dataProvider}
     layout={AdminLayout}
   >
-    <Resource name="migrations" list={MigrationList} icon={MigrationIcon} recordRepresentation="migration" />
+    <Resource name="migrations"
+      list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
